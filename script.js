e00658502f340d8292de74845a79a9bfd98649a0
ProgrammingParadox <theparadox31415@gmail.com>
2022-05-05 21:53:52 -0700

    update stuff

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let nav = fetch('./nav.html')
   .then(response => (new DOMParser).parseFromString(response.text(), "text/html")) //.getElementById("nav")
   .then(element => {
-    console.log(element);
+    console.log(element, element.getElementById);
 
     // document.getElementById("nav").replaceWith(element);
   });
