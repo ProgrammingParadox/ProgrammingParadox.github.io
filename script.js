4d93f10ce70d312d5af8b726c6ea9741147bed4a
ProgrammingParadox <theparadox31415@gmail.com>
2022-05-05 20:50:31 -0700

    Add nav bar

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// get the nav bar's html
+let nav = fetch('./nav.html')
+  .then(response => new DOMParser.parseFromString(response.text(), "text/html")).getElementById("nav")
+  .then(element => {
+    document.getElementById("nav").replaceWith(element);
+  });
