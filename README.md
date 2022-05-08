6f0671160673a27683238de718afa74649cf0403
ProgrammingParadox <theparadox31415@gmail.com>
2022-05-07 18:27:00 -0700

    I think I just broke everything
    Maybe it will be OK?

--- README.md
+++
@@ -1,1 +0,0 @@
-# ProgrammingParadox.github.io
--- script.js
+++
@@ -1,9 +0,0 @@
-
-// get the nav bar's html
-let nav = fetch('./nav.html')
-  .then(response => (new DOMParser).parseFromString(response.text(), "text/html")) //.getElementById("nav")
-  .then(element => {
-    console.log(element, element.getElementById);
-
-    // document.getElementById("nav").replaceWith(element);
-  });
